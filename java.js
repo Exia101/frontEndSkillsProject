6b5cfb8dde17e0f178960eeb36f7f6e0d35a34c6
Pratik Giri <pratik_giri@live.co.uk>
2016-11-07 15:00:17 +0000

    linked sass and did basic design

---
+++ java.js
@@ -0,0 +1,15 @@
+$(function(){
+
+//Google API key= AIzaSyCVeGzs-Q61hD3r8dW_KzLTTwabLHztBi4
+
+
+  // function myMap() {
+  //   var mapCanvas = document.getElementById("map");
+  //   var mapOptions = {
+  //     center: new google.maps.LatLng(51.5, -0.2),
+  //     zoom: 10
+  //   }
+  //   var map = new google.maps.Map(mapCanvas, mapOptions);
+  // }
+
+});
