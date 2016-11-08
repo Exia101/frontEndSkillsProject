3fbd2b66e6d87f818330a50cb38ee7d0adb711f0
Pratik Giri <pratik_giri@live.co.uk>
2016-11-08 21:24:34 +0000

    got geocoder working

--- java.js
+++ java.js
@@ -1,15 +1,33 @@
 $(function(){
+  function showResult(result) {
+    document.getElementById('latitude').value = result.geometry.location.lat();
+    document.getElementById('longitude').value = result.geometry.location.lng();
+  }
 
-//Google API key= AIzaSyCVeGzs-Q61hD3r8dW_KzLTTwabLHztBi4
 
 
-  // function myMap() {
-  //   var mapCanvas = document.getElementById("map");
-  //   var mapOptions = {
-  //     center: new google.maps.LatLng(51.5, -0.2),
-  //     zoom: 10
-  //   }
-  //   var map = new google.maps.Map(mapCanvas, mapOptions);
-  // }
+  function getLatitudeLongitude(callback, address) {
+    // If adress is not supplied, use default value 'Ferrol, Galicia, Spain'
+    address = address;
+    // Initialize the Geocoder
+    geocoder = new google.maps.Geocoder();
+    if (geocoder) {
+      geocoder.geocode({
+        'address': address
+      }, function (results, status) {
+        if (status == google.maps.GeocoderStatus.OK) {
+          callback(results[0]);
+        }
+      });
+    }
+  }
 
+  document.getElementById("button101").addEventListener("click", function () {
+    console.log("button Clicked");
+    var address = document.getElementById('address').value;
+    getLatitudeLongitude(showResult, address)
+  });
 });
+
+// client_id: 9db7adbec9ef444e959a2962041a3370
+// access token 4133909671.9db7adb.96a61d1d52ff4a38bb7d8441a97ce50a
