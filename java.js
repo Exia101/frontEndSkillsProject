0241c4a684c195fc059a5a0135461e3b2fa4137f
Pratik Giri <pratik_giri@live.co.uk>
2016-11-09 14:57:31 +0000

    got callback from flickr working

--- java.js
+++ java.js
@@ -1,33 +1,65 @@
-$(function(){
-  function showResult(result) {
-    document.getElementById('latitude').value = result.geometry.location.lat();
-    document.getElementById('longitude').value = result.geometry.location.lng();
-  }
+
+//   function showResult(result) {
+//     document.getElementById('latitude').value = result.geometry.location.lat();
+//     document.getElementById('longitude').value = result.geometry.location.lng();
+//   }
+//
+//   function getLatitudeLongitude(callback, address) {
+//     address = address;
+//     // Initialize the Geocoder
+//     geocoder = new google.maps.Geocoder();
+//     if (geocoder) {
+//       geocoder.geocode({
+//         'address': address
+//       }, function (results, status) {
+//         if (status == google.maps.GeocoderStatus.OK) {
+//           callback(results[0]);
+//         }
+//       });
+//     }
+//   }
+//
+//   document.getElementById("button101").addEventListener("click", function () {
+//     console.log("button Clicked");
+//     var address = document.getElementById('address').value;
+//     getLatitudeLongitude(showResult, address)
+  // });
 
 
+  function jsonFlickrFeed(json) {
+    $.each(json.items, function(i, item) {
+      $("<img />").attr("src", item.media.m).appendTo("#images");
+    });
+  };
 
-  function getLatitudeLongitude(callback, address) {
-    // If adress is not supplied, use default value 'Ferrol, Galicia, Spain'
-    address = address;
-    // Initialize the Geocoder
-    geocoder = new google.maps.Geocoder();
-    if (geocoder) {
-      geocoder.geocode({
-        'address': address
-      }, function (results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          callback(results[0]);
-        }
-      });
-    }
-  }
+  $("button").click(function(){
 
-  document.getElementById("button101").addEventListener("click", function () {
-    console.log("button Clicked");
-    var address = document.getElementById('address').value;
-    getLatitudeLongitude(showResult, address)
-  });
-});
+    $.ajax({
+      url: 'https://api.flickr.com/services/feeds/photos_public.gne',
+      dataType: 'jsonp',
+      data: { "tags": "", "format": "json" }
+    });
+  })
 
-// client_id: 9db7adbec9ef444e959a2962041a3370
+
+// function jsonFlickrFeed(json) {
+//   console.log(json);
+//
+//   $.each(json.items, function(i, item) {
+//     $("<img />").attr("src", item.media.m).appendTo("#images");
+//   });
+// };
+//
+// function handleButtonClick() {
+//   $("button").remove();
+//
+//   $.ajax({
+//     url: 'https://api.flickr.com/services/feeds/photos_public.gne',
+//     dataType: 'jsonp',
+//     data: { "tags": "kitten", "format": "json" }
+//   });
+// }
+
+
+// flickr_key: 9bcc690e1a00968109fe8d9b8f69e65f
 // access token 4133909671.9db7adb.96a61d1d52ff4a38bb7d8441a97ce50a
